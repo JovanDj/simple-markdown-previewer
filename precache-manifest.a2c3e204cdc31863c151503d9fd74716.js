7b8a8da011567125e98b4047ced59e48cc6125ae
Jovan <jovandjukic2@gmail.com>
2019-11-06 13:23:39 +0100

    Updates

---
+++ precache-manifest.a2c3e204cdc31863c151503d9fd74716.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b017a498b804cb17c53fc161fd15ce1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "becd894bc5c135d19b98",
+    "url": "/static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "a42aa6bb6b13be7b05c9",
+    "url": "/static/css/main.4281cf23.chunk.css"
+  },
+  {
+    "revision": "becd894bc5c135d19b98",
+    "url": "/static/js/2.7be6efa6.chunk.js"
+  },
+  {
+    "revision": "a42aa6bb6b13be7b05c9",
+    "url": "/static/js/main.440b8aef.chunk.js"
+  },
+  {
+    "revision": "05da383847adddd2a0e4",
+    "url": "/static/js/runtime-main.c9ef70d2.js"
+  }
+]);
